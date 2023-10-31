78fa9a27ccf8def9ecf4904dde8e426e79fff4af
Musa Kavus <musakavus@gmail.com>
2023-10-31 22:07:19 +0300

    ignore rule for mse

--- render_test/urls.py
+++ render_test/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for render_test project.
+URL configuration for musakavustech project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
