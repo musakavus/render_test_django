cc02eac9e9864005254597b0b25f599baca800b0
Musa <musakavus@gmail.com>
2024-01-02 23:35:48 +0300

    test

--- render_test/settings.py
+++ render_test/settings.py
@@ -58,14 +58,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'render_test.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
-database_url = os.environ.get('DATABASE_URL')
-DATABASES["default"] = dj_database_url.parse(database_url)
+# database_url = os.environ.get('DATABASE_URL')
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
