68237109b7f103725864bc39678b5541de06c04a
Musa <musakavus@gmail.com>
2024-01-03 01:26:09 +0300

    touchedd

--- render_test/settings.py
+++ render_test/settings.py
@@ -58,19 +58,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'render_test.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get(
-        'postgres://render_test_django_user:CrLCVsSJb7y08sTQSjOVwLxi5qkV6XAV@dpg-ckvc703amefc73f5rvf0-a.frankfurt-postgres.render.com/render_test_django'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-# database_url = os.environ.get('DATABASE_URL')
-# DATABASES["default"] = dj_database_url.parse(database_url)
+
+database_url = os.environ.get('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
