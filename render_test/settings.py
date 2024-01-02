3b08f47f9a50b3584fd52578405e6965dc7bc14b
Musa <musakavus@gmail.com>
2024-01-03 00:15:36 +0300

    touched

--- render_test/settings.py
+++ render_test/settings.py
@@ -90,12 +90,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
-STATIC_ROOT = (BASE_DIR / 'staficfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
