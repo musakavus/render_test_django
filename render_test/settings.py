c96ccac08c96636ac58e57037f9ababc1d2777b5
Musa <musakavus@gmail.com>
2024-01-03 04:39:40 +0300

    control

--- render_test/settings.py
+++ render_test/settings.py
@@ -1,14 +1,24 @@
 from pathlib import Path
 import dj_database_url
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 from django.contrib import staticfiles
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# burası git işleminde açılacak
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
+# ALLOWED_HOSTS = ['www.musakavus.dev', '*', 'localhost', '0.0.0.0']
+# DEBUG = True
+# SECRET_KEY = 'django-insecure-hxe@e^h8-(x7iij$w#wy6cliu5#r70kz#fm+8_79y9r-6=^-%s'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -54,7 +64,7 @@ WSGI_APPLICATION = 'render_test.wsgi.application'
 
 database_url = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(database_url)
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
